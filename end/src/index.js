45a7c55db2b476c6fc94b08375952a11ff381c0b
Francesco Orsi <hello@orsi.me>
2021-01-27 12:25:52 +0100

    added custom alert

--- end/src/index.js
+++ end/src/index.js
@@ -3,7 +3,11 @@ import $ from 'jquery';
 import loaded from '../src/assets/js/loaded';
 import scroll from '../src/assets/js/scroll';
 import resize from '../src/assets/js/resize';
+import alertEl from '../src/assets/js/alertEl';
 import routes from '../src/assets/js/routes';
+
+import test from '../src/assets/js/test';
+test();
 
 const isLive = location.hostname != 'localhost';
 const apiUrl = isLive ? process.env.APIURL : process.env.APIURLDEBUG;
@@ -15,5 +19,8 @@ $(window).on('load', () => { loaded(); });
 $(window).scroll(() => { scroll(); });
 
 $(window).resize(() => { resize(); });
+
+alert = function (text) { alertEl(text); }
+confirm = function (text, callback) { alertEl(text, callback); }
 
 if (module.hot) { module.hot.accept() }
